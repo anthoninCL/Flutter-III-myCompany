4e8220b07f4bc8ab6c38704e6503e7f9f9ceda81
Anthonin Clara <anthonin.clara@epitech.eu>
2021-11-17 17:46:07 +0100

    feat: init project

---
+++ mycompany/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mycompany/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ mycompany/lib/src/app.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello"),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
