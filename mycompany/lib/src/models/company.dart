cb3a63e351644b4b016c512e7bc236415a3bf110
Lucas Fabreguettes <lucas31ff@gmail.com>
2021-12-03 12:25:09 +0100

    feat: Add company CRUD

---
+++ mycompany/lib/src/models/company.dart
@@ -0,0 +1,44 @@
+import 'package:uuid/uuid.dart';
+
+class Company {
+  String id;
+  String name;
+  String address;
+  String zipCode;
+  String city;
+  String country;
+  String contact;
+  String phoneNumber;
+  List<String> users;
+  List<String> poles;
+
+  Company(this.id, this.name, this.address, this.zipCode, this.city,
+      this.country, this.contact, this.phoneNumber, this.users, this.poles);
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "name": name,
+      "address": address,
+      "zipCode": zipCode,
+      "city": city,
+      "country": country,
+      "contact": contact,
+      "phoneNumber": phoneNumber,
+      "users": users,
+      "poles": poles,
+    };
+  }
+
+  Company.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        name = map['name'],
+        address = map['address'],
+        zipCode = map["zipCode"],
+        city = map['city'],
+        country = map['country'],
+        contact = map['contact'],
+        phoneNumber = map["phoneNumber"],
+        users = map["users"],
+        poles = map["poles"];
+}
---
+++ mycompany/lib/src/services/company_service.dart
@@ -0,0 +1,34 @@
+import 'package:mycompany/src/models/company.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class CompanyService {
+  CollectionReference companies =
+      FirebaseFirestore.instance.collection('companies');
+
+  //Add & Update Method
+  Future<void> setCompany(Company company) {
+    return (companies
+        .doc(company.id)
+        .set(company.toMap())
+        .catchError((error) => print(error)));
+  }
+
+  //Delete
+  Future<void> deleteCompany(String companyId) {
+    return (companies
+        .doc(companyId)
+        .delete()
+        .catchError((error) => print(error)));
+  }
+
+  //Read
+
+  Future<Company> readCompany(String companyId) async {
+    var collection = FirebaseFirestore.instance.collection('companies');
+    var docSnapshot = await collection.doc('doc_id').get();
+    Map<String, dynamic> data = docSnapshot.data()!;
+
+    return (Company.fromMap(data));
+  }
+}
