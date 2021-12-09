afc3649ec35fef0d32bd7f110e427812a7af2e8e
Anthonin Clara <anthonin.clara@epitech.eu>
2021-12-09 16:53:17 +0100

    fix: user form example

--- mycompany_admin/lib/src/widgets/forms/user_form.dart
+++ mycompany_admin/lib/src/widgets/forms/user_form.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mycompany_admin/src/widgets/form_basic_input.dart';
 import 'package:mycompany_admin/src/widgets/form_layout.dart';
 
-// TODO This is just an example
-
 class UserForm extends StatefulWidget {
   const UserForm({Key? key}) : super(key: key);
 
@@ -14,17 +12,47 @@ class UserForm extends StatefulWidget {
 }
 
 class _UserFormState extends State<UserForm> {
+  final TextEditingController _firstNameTextController = TextEditingController();
+  final TextEditingController _lastNameTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
+  final TextEditingController _addressTextController = TextEditingController();
+  final TextEditingController _zipCodeTextController = TextEditingController();
   final TextEditingController _cityTextController = TextEditingController();
+  final TextEditingController _countryTextController = TextEditingController();
+  final TextEditingController _phoneNumberTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return FormLayout(children: [
       FormBasicInput(
-        readOnly: true,
+        readOnly: false,
+        fieldTitle: "FirstName",
+        textEditingController: _firstNameTextController,
+        hintText: "FirstName",
+      ),
+      FormBasicInput(
+        readOnly: false,
+        fieldTitle: "LastName",
+        textEditingController: _lastNameTextController,
+        hintText: "LastName",
+      ),
+      FormBasicInput(
+        readOnly: false,
         fieldTitle: "Email",
         textEditingController: _emailTextController,
-        hintText: "Fill with an email",
+        hintText: "Email",
+      ),
+      FormBasicInput(
+        readOnly: false,
+        fieldTitle: "Address",
+        textEditingController: _addressTextController,
+        hintText: "Address",
+      ),
+      FormBasicInput(
+        readOnly: false,
+        fieldTitle: "ZipCode",
+        textEditingController: _zipCodeTextController,
+        hintText: "ZipCode",
       ),
       FormBasicInput(
         readOnly: false,
@@ -32,6 +60,21 @@ class _UserFormState extends State<UserForm> {
         textEditingController: _cityTextController,
         hintText: "Fill with a city",
       ),
+      FormBasicInput(
+        readOnly: false,
+        fieldTitle: "Country",
+        textEditingController: _countryTextController,
+        hintText: "Country",
+      ),
+      FormBasicInput(
+        readOnly: false,
+        fieldTitle: "Phone number",
+        textEditingController: _phoneNumberTextController,
+        hintText: "Phone number",
+      ),
+      // TODO RoleSelectInput
+      // TODO ProjectSelectInput
+      // TODO PoleSelectInput
     ]);
   }
 }
