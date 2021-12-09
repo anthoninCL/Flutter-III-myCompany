ce57a1a4ad2e74e0fc31af4a3c9830b87a020c1f
Anthonin Clara <anthonin.clara@epitech.eu>
2021-12-09 16:53:44 +0100

    feat: add meeting form preview

---
+++ mycompany_admin/lib/src/widgets/forms/meeting_form.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:mycompany_admin/src/widgets/form_basic_input.dart';
+import 'package:mycompany_admin/src/widgets/form_layout.dart';
+
+class MeetingForm extends StatefulWidget {
+  const MeetingForm({Key? key}) : super(key: key);
+
+  @override
+  _MeetingFormState createState() => _MeetingFormState();
+}
+
+class _MeetingFormState extends State<MeetingForm> {
+  final TextEditingController _nameTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return FormLayout(children: [
+      FormBasicInput(
+        readOnly: false,
+        fieldTitle: "Project name",
+        textEditingController: _nameTextController,
+        hintText: "Project name",
+      ),
+      // TODO UserSelectInput
+      // TODO DateTimeInput
+      // TODO MeetingDurationInput
+    ]);
+  }
+}
