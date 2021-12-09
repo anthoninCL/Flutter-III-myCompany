807a343011fd9bd57d96ceeb90c67f354c75b227
Anthonin Clara <anthonin.clara@epitech.eu>
2021-12-09 15:38:26 +0100

    feat: add basic input widget

---
+++ mycompany_admin/lib/src/widgets/form_basic_input.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class FormBasicInput extends StatefulWidget {
+  const FormBasicInput(
+      {Key? key,
+      required this.enable,
+      required this.fieldTitle,
+      required this.textEditingController,
+      this.defaultValue,
+      this.hintText})
+      : super(key: key);
+
+  final bool enable;
+  final String fieldTitle;
+  final TextEditingController textEditingController;
+  final String? defaultValue;
+  final String? hintText;
+
+  @override
+  _FormBasicInputState createState() => _FormBasicInputState();
+}
+
+class _FormBasicInputState extends State<FormBasicInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          widget.fieldTitle,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        const SizedBox(
+          width: 30,
+        ),
+        Container(
+          height: 50,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.only(left: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Colors.black)),
+          child: TextField(
+            controller: widget.textEditingController,
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: widget.hintText,
+                hintStyle: const TextStyle(
+                  color: Colors.grey,
+                  fontSize: 30,
+                )),
+          ),
+        )
+      ],
+    );
+  }
+}
